d90f0e939c29a87dd2481b5a2642ed5f2ee86b56
Raj Beemi <beemi.raja@gmail.com>
2024-01-21 06:09:49 +0000

    feat: swagger docs

--- README.md
+++ README.md
@@ -22,3 +22,11 @@ mvn spring-boot:run
 ```shell
 docker-compose up -d
 ```
+
+## Swagger
+
+Access Open API documentation at: http://localhost:8090/swagger-ui/index.html
+
+API documentation is also available in JSON format at: http://localhost:8090/v3/api-docs
+
+![Swagger UI](docs/images/swagger-api.png)
--- src/main/java/com/jaitechltd/bookservice/controller/BookController.java
+++ src/main/java/com/jaitechltd/bookservice/controller/BookController.java
@@ -2,12 +2,10 @@ package com.jaitechltd.bookservice.controller;
 
 import com.jaitechltd.bookservice.model.Book;
 import com.jaitechltd.bookservice.service.BookServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -20,25 +18,35 @@ public class BookController {
         this.bookServiceImpl = bookServiceImpl;
     }
 
-    @RequestMapping("/create")
+    @PostMapping("/create")
+    @Operation(summary = "Create a new book", description = "Create a new book", tags = { "book" },
+            operationId = "createBook", responses = { @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "200", description = "Book created") })
     public ResponseEntity<Book> createBook(@RequestBody Book book) {
         log.info("createBook ...");
         return ResponseEntity.ok(bookServiceImpl.createBook(book));
     }
 
-    @RequestMapping("/{id}")
+    @GetMapping("/{id}")
+    @Operation(summary = "Get a book by id", description = "Get a book by id", tags = { "book" },
+            operationId = "getBook", responses = { @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "200", description = "Book found") })
     public ResponseEntity<Book> getBook(@PathVariable("id") Long id) {
         log.info("getBook ...");
         return ResponseEntity.ok(bookServiceImpl.getBook(id));
     }
 
-    @RequestMapping("/all")
+    @GetMapping("/all")
+    @Operation(summary = "Get all books", description = "Get all books", tags = { "book" },
+            operationId = "getAllBooks")
     public ResponseEntity<Iterable<Book>> getAllBooks() {
         log.info("getAllBooks ...");
         return ResponseEntity.ok(bookServiceImpl.getAllBooks());
     }
 
-    @RequestMapping("/{id}/delete")
+    @DeleteMapping("/{id}/delete")
+    @Operation(summary = "Delete a book by id", description = "Delete a book by id", tags = { "book" },
+            operationId = "deleteBook")
     public ResponseEntity<String> deleteBook(@PathVariable("id") Long id) {
         log.info("deleteBook ...");
         bookServiceImpl.deleteBook(id);
